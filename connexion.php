3a440ab57f22807020cf374c5a4782957014bc35
mathieu-tatat <mathieu.tatat@laplateforme.io>
2022-05-02 09:22:05 +0200

    modif connection db

--- connexion.php
+++ connexion.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-    $db = new PDO('mysql:host=localhost;dbname=autocompletion;charset=utf8' , 'root', 'root');
+    $db = new PDO('mysql:host=localhost:3306;dbname=mathieu-tatat_autocompletion;charset=utf8' , 'barresearch', 'barresearch');
 }
 catch (Exception $e) {
     die('error : ' . $e->getMessage());
